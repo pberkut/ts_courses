92a8c6a762d5d224188a17890a0591ab7acb03cb
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-07-27 14:42:21 +0300

    bugfix

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ const button = document.querySelector('button');
 const input = document.querySelector('input');
 if (button && input) {
     button.addEventListener('click', () => {
-        concatenation(input.value, 'hello!');
+        concatenation(input.value, 'hello word!!');
     });
 }
+//# sourceMappingURL=index.js.map
